409082618f2efb48442fc24d0422eddee9f9970e
David Stevenson <drsgis@rit.edu>
2024-10-10 13:29:17 -0400

    sting 2.6 Adding the HomeController.cs file in the Controllers folder

---
+++ ch02/ch02_ProASPNETCore7/FirstProject/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿// Listing 2.6 Adding the HomeController.cs file in the Controllers folder
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello World";
+        }
+    }
+}
