03d2a600aeaac493c6fe01dc56b92055a88b6194
David Stevenson <drsgis@rit.edu>
2024-10-10 13:52:28 -0400

    Listing 3.3 The contents of the HomeController.cs file in the Controllers folder

--- ch03/ch03_ProASPNETCore7/PartyInvites/Program.cs
+++ ch03/ch03_ProASPNETCore7/PartyInvites/Program.cs
@@ -7,14 +7,14 @@ namespace PartyInvites
 			var builder = WebApplication.CreateBuilder(args);
 
 			// Add services to the container.
-			builder.Services.AddRazorPages();
+			builder.Services.AddControllersWithViews();
 
 			var app = builder.Build();
 
 			// Configure the HTTP request pipeline.
 			if (!app.Environment.IsDevelopment())
 			{
-				app.UseExceptionHandler("/Error");
+				app.UseExceptionHandler("/Home/Error");
 			}
 			app.UseStaticFiles();
 
@@ -22,7 +22,9 @@ namespace PartyInvites
 
 			app.UseAuthorization();
 
-			app.MapRazorPages();
+			app.MapControllerRoute(
+				name: "default",
+				pattern: "{controller=Home}/{action=Index}/{id?}");
 
 			app.Run();
 		}
