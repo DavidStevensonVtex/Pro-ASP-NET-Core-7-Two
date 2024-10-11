f0a47df90e812cf3e2c8caa686ebe87f36dcf2bc
David Stevenson <drsgis@rit.edu>
2024-10-11 13:20:10 -0400

    Listing 5.7 Running the example application
    dotnet run

---
+++ ch05/Listing_5-07.ps1
@@ -0,0 +1,17 @@
+# Listing 5.7 Running the example application
+
+dotnet run 
+
+# PS C:\Users\drsgis\drs\Pro-ASP-NET-Core-7-Two\ch05\LanguageFeatures> ..\Listing_5-07.ps1
+# Building...
+# C:\Users\drsgis\drs\Pro-ASP-NET-Core-7-Two\ch05\LanguageFeatures\Models\Product.cs(7,17): warning CS8618: Non-nullable property 'Name' mu
+# st contain a non-null value when exiting constructor. Consider declaring the property as nullable. [C:\Users\drsgis\drs\Pro-ASP-NET-Core-
+# 7-Two\ch05\LanguageFeatures\LanguageFeatures.csproj]
+# info: Microsoft.Hosting.Lifetime[14]
+#       Now listening on: http://localhost:5000
+# info: Microsoft.Hosting.Lifetime[0]
+#       Application started. Press Ctrl+C to shut down.
+# info: Microsoft.Hosting.Lifetime[0]
+#       Hosting environment: Development
+# info: Microsoft.Hosting.Lifetime[0]
+#       Content root path: C:\Users\drsgis\drs\Pro-ASP-NET-Core-7-Two\ch05\LanguageFeatures
