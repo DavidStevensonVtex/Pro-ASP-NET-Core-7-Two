c9fc08fea17c3179aab95f1ca28bd0aad0358747
David Stevenson <drsgis@rit.edu>
2024-10-11 09:39:54 -0400

    Listing 4.18 Installing the Bootstrap CSS framework
    libman install bootstrap@5.3.3 -d wwwroot/lib/bootstrap

---
+++ ch04/Listing_4-18.ps1
@@ -0,0 +1,6 @@
+# Listing 4.18 Installing the Bootstrap CSS framework
+
+libman install bootstrap@5.3.3 -d wwwroot/lib/bootstrap 
+
+# ...
+# Installed library "bootstrap@5.3.3" to "wwwroot/lib/bootstrap"
