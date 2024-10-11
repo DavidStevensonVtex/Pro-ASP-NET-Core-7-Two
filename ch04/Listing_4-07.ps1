6f90e8b0f9710601eac3ce61188d3f1910988aef
David Stevenson <drsgis@rit.edu>
2024-10-11 09:03:32 -0400

    Listing 4.7 Building and running the project

---
+++ ch04/Listing_4-07.ps1
@@ -0,0 +1,20 @@
+# Listing 4.7 Building and running the project
+
+dotnet run
+
+# pwd
+
+# Path
+# ----
+# C:\Users\drsgis\drs\Pro-ASP-NET-Core-7-Two\ch04\MySolution\MyProject
+
+# PS C:\Users\drsgis\drs\Pro-ASP-NET-Core-7-Two\ch04\MySolution\MyProject> ../../Listing_4-07.ps1    
+# Building...
+# info: Microsoft.Hosting.Lifetime[14]
+#       Now listening on: http://localhost:5000
+# info: Microsoft.Hosting.Lifetime[0]
+#       Application started. Press Ctrl+C to shut down.
+# info: Microsoft.Hosting.Lifetime[0]
+#       Hosting environment: Development
+# info: Microsoft.Hosting.Lifetime[0]
+#       Content root path: C:\Users\drsgis\drs\Pro-ASP-NET-Core-7-Two\ch04\MySolution\MyProject
