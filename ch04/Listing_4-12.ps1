5bfdda6e9ff9df61d352f6046440bb82d8e28b93
David Stevenson <drsgis@rit.edu>
2024-10-11 09:22:48 -0400

    Listing 4.12 Listing the packages in a project
    dotnet list package

---
+++ ch04/Listing_4-12.ps1
@@ -0,0 +1,15 @@
+# Listing 4.12 Listing the packages in a project
+
+dotnet list package
+
+# pwd
+
+# Path
+# ----
+# C:\Users\drsgis\drs\Pro-ASP-NET-Core-7-Two\ch04\MySolution\MyProject
+
+# PS C:\Users\drsgis\drs\Pro-ASP-NET-Core-7-Two\ch04\MySolution\MyProject> ../../Listing_4-12.ps1    
+# Project 'MyProject' has the following package references
+#    [net8.0]: 
+#    Top-level Package                              Requested   Resolved
+#    > Microsoft.EntityFrameworkCore.SqlServer      8.0.10      8.0.10
