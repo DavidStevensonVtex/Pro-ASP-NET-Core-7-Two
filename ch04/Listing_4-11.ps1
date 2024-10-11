2c7c613d7523a21b62bab7d43addd5003155c05e
David Stevenson <drsgis@rit.edu>
2024-10-11 09:19:49 -0400

    Listing 4.11 Adding a package to the example project
    dotnet add package Microsoft.EntityFrameworkCore.SqlServer --version 8.0.10

---
+++ ch04/Listing_4-11.ps1
@@ -0,0 +1,79 @@
+# Listing 4.11 Adding a package to the example project
+
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer --version 8.0.10
+
+# pwd                       
+
+# Path
+# ----
+# C:\Users\drsgis\drs\Pro-ASP-NET-Core-7-Two\ch04\MySolution\MyProject
+
+# PS C:\Users\drsgis\drs\Pro-ASP-NET-Core-7-Two\ch04\MySolution\MyProject> ../../Listing_4-11.ps1    
+#   Determining projects to restore...
+#   Writing C:\Users\drsgis\AppData\Local\Temp\tmpnacctu.tmp
+# info : X.509 certificate chain validation will use the default trust store selected by .NET for code signing.
+# info : X.509 certificate chain validation will use the default trust store selected by .NET for timestamping.
+# info : Adding PackageReference for package 'Microsoft.EntityFrameworkCore.SqlServer' into project 'C:\Users\drsgis\drs\Pro-ASP-NET-Core-7-Two\ch04\MySolution\MyProject\MyProject.csproj'.  
+# info : Restoring packages for C:\Users\drsgis\drs\Pro-ASP-NET-Core-7-Two\ch04\MySolution\MyProject\MyProject.csproj...
+# info :   GET https://api.nuget.org/v3-flatcontainer/microsoft.entityframeworkcore.sqlserver/index.json
+# info :   OK https://api.nuget.org/v3-flatcontainer/microsoft.entityframeworkcore.sqlserver/index.json 194ms
+# info :   GET https://api.nuget.org/v3-flatcontainer/microsoft.entityframeworkcore.sqlserver/8.0.10/microsoft.entityframeworkcore.sqlserver.8.0.10.nupkg
+# info :   OK https://api.nuget.org/v3-flatcontainer/microsoft.entityframeworkcore.sqlserver/8.0.10/microsoft.entityframeworkcore.sqlserver.8.0.10.nupkg 55ms
+# info :   GET https://api.nuget.org/v3-flatcontainer/microsoft.entityframeworkcore.relational/index.json
+# info :   OK https://api.nuget.org/v3-flatcontainer/microsoft.entityframeworkcore.relational/index.json 53ms
+# info :   GET https://api.nuget.org/v3-flatcontainer/microsoft.entityframeworkcore.relational/8.0.10/microsoft.entityframeworkcore.relational.8.0.10.nupkg
+# info :   OK https://api.nuget.org/v3-flatcontainer/microsoft.entityframeworkcore.relational/8.0.10/microsoft.entityframeworkcore.relational.8.0.10.nupkg 14ms
+# info :   GET https://api.nuget.org/v3-flatcontainer/microsoft.entityframeworkcore/index.json
+# info :   OK https://api.nuget.org/v3-flatcontainer/microsoft.entityframeworkcore/index.json 45ms
+# info :   GET https://api.nuget.org/v3-flatcontainer/microsoft.entityframeworkcore/8.0.10/microsoft.entityframeworkcore.8.0.10.nupkg
+# info :   OK https://api.nuget.org/v3-flatcontainer/microsoft.entityframeworkcore/8.0.10/microsoft.entityframeworkcore.8.0.10.nupkg 14ms
+# info :   GET https://api.nuget.org/v3-flatcontainer/microsoft.entityframeworkcore.abstractions/index.json
+# info :   GET https://api.nuget.org/v3-flatcontainer/microsoft.entityframeworkcore.analyzers/index.json
+# info :   GET https://api.nuget.org/v3-flatcontainer/microsoft.extensions.caching.memory/index.json
+# info :   GET https://api.nuget.org/v3-flatcontainer/microsoft.extensions.logging/index.json
+# info :   OK https://api.nuget.org/v3-flatcontainer/microsoft.entityframeworkcore.abstractions/index.json 44ms
+# info :   GET https://api.nuget.org/v3-flatcontainer/microsoft.entityframeworkcore.abstractions/8.0.10/microsoft.entityframeworkcore.abstractions.8.0.10.nupkg
+# info :   OK https://api.nuget.org/v3-flatcontainer/microsoft.entityframeworkcore.analyzers/index.json 87ms
+# info :   OK https://api.nuget.org/v3-flatcontainer/microsoft.entityframeworkcore.abstractions/8.0.10/microsoft.entityframeworkcore.abstractions.8.0.10.nupkg 41ms
+# info :   OK https://api.nuget.org/v3-flatcontainer/microsoft.extensions.logging/index.json 85ms
+# info :   GET https://api.nuget.org/v3-flatcontainer/microsoft.entityframeworkcore.analyzers/8.0.10/microsoft.entityframeworkcore.analyzers.8.0.10.nupkg
+# info :   GET https://api.nuget.org/v3-flatcontainer/microsoft.extensions.logging/8.0.1/microsoft.extensions.logging.8.0.1.nupkg
+# info :   OK https://api.nuget.org/v3-flatcontainer/microsoft.entityframeworkcore.analyzers/8.0.10/microsoft.entityframeworkcore.analyzers.8.0.10.nupkg 27ms
+# info :   OK https://api.nuget.org/v3-flatcontainer/microsoft.extensions.logging/8.0.1/microsoft.extensions.logging.8.0.1.nupkg 24ms
+# info :   OK https://api.nuget.org/v3-flatcontainer/microsoft.extensions.caching.memory/index.json 153ms
+# info :   GET https://api.nuget.org/v3-flatcontainer/microsoft.extensions.caching.memory/8.0.1/microsoft.extensions.caching.memory.8.0.1.nupkg
+# info :   GET https://api.nuget.org/v3-flatcontainer/microsoft.extensions.dependencyinjection/index.json
+# info :   GET https://api.nuget.org/v3-flatcontainer/microsoft.extensions.logging.abstractions/index.json
+# info :   OK https://api.nuget.org/v3-flatcontainer/microsoft.extensions.caching.memory/8.0.1/microsoft.extensions.caching.memory.8.0.1.nupkg 14ms
+# info :   GET https://api.nuget.org/v3-flatcontainer/microsoft.extensions.dependencyinjection.abstractions/index.json
+# info :   OK https://api.nuget.org/v3-flatcontainer/microsoft.extensions.dependencyinjection/index.json 42ms
+# info :   GET https://api.nuget.org/v3-flatcontainer/microsoft.extensions.dependencyinjection/8.0.1/microsoft.extensions.dependencyinjection.8.0.1.nupkg
+# info :   OK https://api.nuget.org/v3-flatcontainer/microsoft.extensions.logging.abstractions/index.json 46ms
+# info :   GET https://api.nuget.org/v3-flatcontainer/microsoft.extensions.logging.abstractions/8.0.2/microsoft.extensions.logging.abstractions.8.0.2.nupkg
+# info :   OK https://api.nuget.org/v3-flatcontainer/microsoft.extensions.dependencyinjection/8.0.1/microsoft.extensions.dependencyinjection.8.0.1.nupkg 15ms
+# info :   OK https://api.nuget.org/v3-flatcontainer/microsoft.extensions.logging.abstractions/8.0.2/microsoft.extensions.logging.abstractions.8.0.2.nupkg 14ms
+# info :   OK https://api.nuget.org/v3-flatcontainer/microsoft.extensions.dependencyinjection.abstractions/index.json 49ms
+# info :   GET https://api.nuget.org/v3-flatcontainer/microsoft.extensions.dependencyinjection.abstractions/8.0.2/microsoft.extensions.dependencyinjection.abstractions.8.0.2.nupkg
+# info :   OK https://api.nuget.org/v3-flatcontainer/microsoft.extensions.dependencyinjection.abstractions/8.0.2/microsoft.extensions.dependencyinjection.abstractions.8.0.2.nupkg 16ms       
+# info : Installed Microsoft.EntityFrameworkCore.Analyzers 8.0.10 from https://api.nuget.org/v3/index.json to C:\Users\drsgis\.nuget\packages\microsoft.entityframeworkcore.analyzers\8.0.10 with content hash 51KkPIc0EMv/gVXhPIUi6cwJE9Mvh+PLr4Lap4naLcsoGZ0lF2SvOPgUUprwRV3MnN7nyD1XPhT5RJ/p+xFAXw==.
+# info : Installed Microsoft.EntityFrameworkCore.SqlServer 8.0.10 from https://api.nuget.org/v3/index.json to C:\Users\drsgis\.nuget\packages\microsoft.entityframeworkcore.sqlserver\8.0.10 with content hash DvhBEk44UjWMebFKwIFDIdEsG8gzbgflWIZljDCpIkZVpId+PKs0ufzJxnTQ94InPO+pS7+wE45cRsPRt9B0Iw==.
+# info : Installed Microsoft.EntityFrameworkCore.Abstractions 8.0.10 from https://api.nuget.org/v3/index.json to C:\Users\drsgis\.nuget\packages\microsoft.entityframeworkcore.abstractions\8.0.10 with content hash FV0QlcX9INY4kAD2o72uPtyOh0nZut2jB11Jf9mNYBtHay8gDLe+x4AbXFwuQg+eSvofjT7naV82e827zGfyMg==.
+# info : Installed Microsoft.EntityFrameworkCore.Relational 8.0.10 from https://api.nuget.org/v3/index.json to C:\Users\drsgis\.nuget\packages\microsoft.entityframeworkcore.relational\8.0.10 with content hash OefBEE47kGKPRPV3OT+FAW6o5BFgLk2D9EoeWVy7NbOepzUneayLQxbVE098FfedTyMwxvZQoDD9LrvZc3MadA==.
+# info : Installed Microsoft.EntityFrameworkCore 8.0.10 from https://api.nuget.org/v3/index.json to C:\Users\drsgis\.nuget\packages\microsoft.entityframeworkcore\8.0.10 with content hash PPkQdIqfR1nU3n6YgGGDk8G+eaYbaAKM1AzIQtlPNTKf10Osg3N9T+iK9AlnSA/ujsK00flPpFHVfJrbuBFS1A==.
+# info : Installed Microsoft.Extensions.Caching.Memory 8.0.1 from https://api.nuget.org/v3/index.json to C:\Users\drsgis\.nuget\packages\microsoft.extensions.caching.memory\8.0.1 with content hash HFDnhYLccngrzyGgHkjEDU5FMLn4MpOsr5ElgsBMC4yx6lJh4jeWO7fHS8+TXPq+dgxCmUa/Trl8svObmwW4QA==.
+# info : Installed Microsoft.Extensions.Logging 8.0.1 from https://api.nuget.org/v3/index.json to C:\Users\drsgis\.nuget\packages\microsoft.extensions.logging\8.0.1 with content hash 4x+pzsQEbqxhNf1QYRr5TDkLP9UsLT3A6MdRKDDEgrW7h1ljiEPgTNhKYUhNCCAaVpQECVQ+onA91PTPnIp6Lw==.
+# info : Installed Microsoft.Extensions.DependencyInjection.Abstractions 8.0.2 from https://api.nuget.org/v3/index.json to C:\Users\drsgis\.nuget\packages\microsoft.extensions.dependencyinjection.abstractions\8.0.2 with content hash 3iE7UF7MQkCv1cxzCahz+Y/guQbTqieyxyaWKhrRO91itI9cOKO76OHeQDahqG4MmW5umr3CcCvGmK92lWNlbg==.
+# info : Installed Microsoft.Extensions.DependencyInjection 8.0.1 from https://api.nuget.org/v3/index.json to C:\Users\drsgis\.nuget\packages\microsoft.extensions.dependencyinjection\8.0.1 with content hash BmANAnR5Xd4Oqw7yQ75xOAYODybZQRzdeNucg7kS5wWKd2PNnMdYtJ2Vciy0QLylRmv42DGl5+AFL9izA6F1Rw==.
+# info : Installed Microsoft.Extensions.Logging.Abstractions 8.0.2 from https://api.nuget.org/v3/index.json to C:\Users\drsgis\.nuget\packages\microsoft.extensions.logging.abstractions\8.0.2 with content hash nroMDjS7hNBPtkZqVBbSiQaQjWRDxITI8Y7XnDs97rqG3EbzVTNLZQf7bIeUJcaHOV8bca47s1Uxq94+2oGdxA==.
+# info :   GET https://api.nuget.org/v3/vulnerabilities/index.json
+# info :   OK https://api.nuget.org/v3/vulnerabilities/index.json 12ms
+# info :   GET https://api.nuget.org/v3-vulnerabilities/2024.10.10.23.29.13/vulnerability.base.json
+# info :   GET https://api.nuget.org/v3-vulnerabilities/2024.10.10.23.29.13/2024.10.11.05.29.13/vulnerability.update.json
+# info :   OK https://api.nuget.org/v3-vulnerabilities/2024.10.10.23.29.13/vulnerability.base.json 13ms
+# info :   OK https://api.nuget.org/v3-vulnerabilities/2024.10.10.23.29.13/2024.10.11.05.29.13/vulnerability.update.json 12ms
+# info : Package 'Microsoft.EntityFrameworkCore.SqlServer' is compatible with all the specified frameworks in project 'C:\Users\drsgis\drs\Pro-ASP-NET-Core-7-Two\ch04\MySolution\MyProject\MyProject.csproj'.
+# info : PackageReference for package 'Microsoft.EntityFrameworkCore.SqlServer' version '8.0.10' added to file 'C:\Users\drsgis\drs\Pro-ASP-NET-Core-7-Two\ch04\MySolution\MyProject\MyProject.csproj'.
+# info : Generating MSBuild file C:\Users\drsgis\drs\Pro-ASP-NET-Core-7-Two\ch04\MySolution\MyProject\obj\MyProject.csproj.nuget.g.props.
+# info : Generating MSBuild file C:\Users\drsgis\drs\Pro-ASP-NET-Core-7-Two\ch04\MySolution\MyProject\obj\MyProject.csproj.nuget.g.targets.
+# info : Writing assets file to disk. Path: C:\Users\drsgis\drs\Pro-ASP-NET-Core-7-Two\ch04\MySolution\MyProject\obj\project.assets.json
+# log  : Restored C:\Users\drsgis\drs\Pro-ASP-NET-Core-7-Two\ch04\MySolution\MyProject\MyProject.csproj (in 1.69 sec).
