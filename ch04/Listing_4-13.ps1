c9bae3a64e00e69f430e4fd32709e64402f70dbf
David Stevenson <drsgis@rit.edu>
2024-10-11 09:25:47 -0400

    Listing 4.13 Removing a package f rom the example project
    dotnet remove package Microsoft.EntityFrameworkCore.sqlserver

---
+++ ch04/Listing_4-13.ps1
@@ -0,0 +1,12 @@
+# Listing 4.13 Removing a package f rom the example project
+
+dotnet remove package Microsoft.EntityFrameworkCore.sqlserver
+
+# pwd
+
+# Path
+# ----
+# C:\Users\drsgis\drs\Pro-ASP-NET-Core-7-Two\ch04\MySolution\MyProject
+
+# PS C:\Users\drsgis\drs\Pro-ASP-NET-Core-7-Two\ch04\MySolution\MyProject> ../../Listing_4-13.ps1
+# info : Removing PackageReference for package 'Microsoft.EntityFrameworkCore.sqlserver' from project 'C:\Users\drsgis\drs\Pro-ASP-NET-Core-7-Two\ch04\MySolution\MyProject\MyProject.csproj'.
