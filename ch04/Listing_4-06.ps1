b4aa12929713bfa1e982280194be89a75fccb791
David Stevenson <drsgis@rit.edu>
2024-10-11 09:00:07 -0400

    Listing 4.6 Buildiing the project
    dotnet build

---
+++ ch04/Listing_4-06.ps1
@@ -0,0 +1,16 @@
+# Listing 4.6 Buildiing the project
+
+dotnet build
+
+# cd MySolution
+# PS C:\Users\drsgis\drs\Pro-ASP-NET-Core-7-Two\ch04\MySolution> ..\Listing_4-06.ps1
+# MSBuild version 17.9.8+b34f75857 for .NET
+#   Determining projects to restore...
+#   All projects are up-to-date for restore.
+#   MyProject -> C:\Users\drsgis\drs\Pro-ASP-NET-Core-7-Two\ch04\MySolution\MyProject\bin\Debug\net8.0\MyProject.dll
+
+# Build succeeded.
+#     0 Warning(s)
+#     0 Error(s)
+
+# Time Elapsed 00:00:04.46
