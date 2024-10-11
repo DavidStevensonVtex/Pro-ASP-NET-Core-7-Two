18571006153ee00b1af9eb5640baf80f30115b4e
David Stevenson <drsgis@rit.edu>
2024-10-11 09:07:01 -0400

    Listing 4.8 Starting the application with hot reload
    dotnet watch

---
+++ ch04/Listing_4-08.ps1
@@ -0,0 +1,26 @@
+# Listing 4.8 Starting the application with hot reload
+
+dotnet watch
+
+# pwd
+
+# Path
+# ----
+# C:\Users\drsgis\drs\Pro-ASP-NET-Core-7-Two\ch04\MySolution\MyProject
+
+# PS C:\Users\drsgis\drs\Pro-ASP-NET-Core-7-Two\ch04\MySolution\MyProject> ../../Listing_4-08.ps1
+# dotnet watch 🔥 Hot reload enabled. For a list of supported edits, see https://aka.ms/dotnet/hot-reload.
+#   💡 Press "Ctrl + R" to restart.
+# dotnet watch 🔧 Building...
+#   Determining projects to restore...
+#   All projects are up-to-date for restore.
+#   MyProject -> C:\Users\drsgis\drs\Pro-ASP-NET-Core-7-Two\ch04\MySolution\MyProject\bin\Debug\net8.0\MyProject.dll
+# dotnet watch 🚀 Started
+# info: Microsoft.Hosting.Lifetime[14]
+#       Now listening on: http://localhost:5000
+# info: Microsoft.Hosting.Lifetime[0]
+#       Application started. Press Ctrl+C to shut down.
+# info: Microsoft.Hosting.Lifetime[0]
+#       Hosting environment: Development
+# info: Microsoft.Hosting.Lifetime[0]
+#       Content root path: C:\Users\drsgis\drs\Pro-ASP-NET-Core-7-Two\ch04\MySolution\MyProject
