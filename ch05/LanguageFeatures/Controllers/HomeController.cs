22ae9e76dca78fdf5e9cbe64d5e5eb8235907094
David Stevenson <drsgis@rit.edu>
2024-10-11 13:13:53 -0400

    Listing 5.4 The contents of the HomeController.cs file in the Controllers folder

---
+++ ch05/LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿// Listing 5.4 The contents of the HomeController.cs file in the Controllers folder
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanguageFeatures.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ViewResult Index()
+		{
+			return View(new string[] { "C#", "Language", "Features" });
+		}
+	}
+}
