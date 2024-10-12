aa9214af12b094beb32ccc72f0fbe94783f93b78
David Stevenson <drstevenson1958@yahoo.com>
2024-10-12 16:08:57 -0400

    Listing 6.7 Running the example application
    dotnet run

---
+++ ch06/Listing_6-07.ps1
@@ -0,0 +1,14 @@
+# Listing 6.7 Running the example application
+
+dotnet run
+
+# PS D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch06\Testing\SimpleApp> ..\..\Listing_6-07.ps1
+# Building...
+# info: Microsoft.Hosting.Lifetime[14]
+#       Now listening on: http://localhost:5000
+# info: Microsoft.Hosting.Lifetime[0]
+#       Application started. Press Ctrl+C to shut down.
+# info: Microsoft.Hosting.Lifetime[0]
+#       Hosting environment: Development
+# info: Microsoft.Hosting.Lifetime[0]
+#       Content root path: D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch06\Testing\SimpleApp
