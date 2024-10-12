6878573385cdbaac9d917f352c31c810b8720309
David Stevenson <drstevenson1958@yahoo.com>
2024-10-12 17:35:22 -0400

    Listing 6.15 The contents of the IDataSource.cs file in the SimpleApp/Models folder

---
+++ ch06/Testing/SimpleApp/Models/IDataSource.cs
@@ -0,0 +1,9 @@
+﻿// Listing 6.15 The contents of the IDataSource.cs file in the SimpleApp/Models folder
+
+namespace SimpleApp.Models
+{
+    public interface IDataSource
+    {
+        IEnumerable<Product> Products { get; }
+    }
+}
