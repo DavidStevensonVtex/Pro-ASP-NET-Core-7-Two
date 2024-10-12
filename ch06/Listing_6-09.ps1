68f146d9353a61065654c96210646d90a435b33d
David Stevenson <drstevenson1958@yahoo.com>
2024-10-12 16:19:14 -0400

    Listing 6.9 Removing the default test class file

---
+++ ch06/Listing_6-09.ps1
@@ -0,0 +1,6 @@
+# Listing 6.9 Removing the default test class file
+
+Remove-Item SimpleApp.Tests/UnitTest1.cs 
+
+# PS D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch06\Testing> ..\Listing_6-09.ps1
+# PS D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch06\Testing> 
