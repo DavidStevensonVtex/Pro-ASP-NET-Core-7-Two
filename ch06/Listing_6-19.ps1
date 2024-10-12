4848dc2427f8878affae627cdeba5c7bdc6fb981
David Stevenson <drstevenson1958@yahoo.com>
2024-10-12 17:56:59 -0400

    Listing 6.19 Installing the mocking package
    dotnet add SimpleApp.Tests package Moq --version 4.18.4

---
+++ ch06/Listing_6-19.ps1
@@ -0,0 +1,18 @@
+# Listing 6.19 Installing the mocking package
+
+dotnet add SimpleApp.Tests package Moq --version 4.18.4
+
+# PS D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch06\Testing> ..\Listing_6-19.ps1
+#   Determining projects to restore...
+#   Writing C:\Users\drste\AppData\Local\Temp\tmprjxofa.tmp
+# info : X.509 certificate chain validation will use the default trust store selected by .NET for code signing.
+# info : X.509 certificate chain validation will use the default trust store selected by .NET for timestamping.
+# info : Adding PackageReference for package 'Moq' into project 'D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch06\Testing\SimpleApp.Tests\SimpleApp.Tests.csproj'.
+# info : Restoring packages for D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch06\Testing\SimpleApp.Tests\SimpleApp.Tests.csproj...
+# info :   CACHE https://api.nuget.org/v3/vulnerabilities/index.json
+# info :   CACHE https://api.nuget.org/v3-vulnerabilities/2024.10.11.23.31.19/vulnerability.base.json
+# info :   CACHE https://api.nuget.org/v3-vulnerabilities/2024.10.11.23.31.19/2024.10.11.23.31.19/vulnerability.update.json
+# info : Package 'Moq' is compatible with all the specified frameworks in project 'D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch06\Testing\SimpleApp.Tests\SimpleApp.Tests.csproj'.
+# info : PackageReference for package 'Moq' version '4.18.4' added to file 'D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch06\Testing\SimpleApp.Tests\SimpleApp.Tests.csproj'.
+# info : Writing assets file to disk. Path: D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch06\Testing\SimpleApp.Tests\obj\project.assets.json
+# log  : Restored D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch06\Testing\SimpleApp.Tests\SimpleApp.Tests.csproj (in 491 ms).
