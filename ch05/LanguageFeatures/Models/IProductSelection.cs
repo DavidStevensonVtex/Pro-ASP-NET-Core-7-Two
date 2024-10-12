184d01776b456203a7b56384a1af15c701a9c770
David Stevenson <drstevenson1958@yahoo.com>
2024-10-11 20:50:27 -0400

    Listing 5.48 The contents of the IProductSelection.cs file in the Models folder

---
+++ ch05/LanguageFeatures/Models/IProductSelection.cs
@@ -0,0 +1,9 @@
+﻿// Listing 5.48 The contents of the IProductSelection.cs file in the Models folder
+
+namespace LanguageFeatures.Models
+{
+    public interface IProductSelection
+    {
+        IEnumerable<Product>? Products { get; }
+    }
+}
