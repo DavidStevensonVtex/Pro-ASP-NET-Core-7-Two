dc718c6fbc9d6923bcf112f1de3a90dbce60eb24
David Stevenson <drstevenson1958@yahoo.com>
2024-10-13 11:01:49 -0400

    Listing 7.3 Installing the Moq package

---
+++ ch07/Listing_7-03.ps1
@@ -0,0 +1,18 @@
+# Listing 7.3 Installing the Moq package
+
+dotnet add SportsSln/SportsStore.Tests package Moq --version 4.18.4
+
+# PS D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch07> .\Listing_7-03.ps1
+#   Determining projects to restore...
+#   Writing C:\Users\drste\AppData\Local\Temp\tmpp1q515.tmp
+# info : X.509 certificate chain validation will use the default trust store selected by .NET for code signing.
+# info : X.509 certificate chain validation will use the default trust store selected by .NET for timestamping.
+# info : Adding PackageReference for package 'Moq' into project 'D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch07\SportsSln\SportsStore.Tests\SportsStore.Tests.csproj'.
+# info : Restoring packages for D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch07\SportsSln\SportsStore.Tests\SportsStore.Tests.csproj...
+# info :   CACHE https://api.nuget.org/v3/vulnerabilities/index.json
+# info :   CACHE https://api.nuget.org/v3-vulnerabilities/2024.10.11.23.31.19/vulnerability.base.json
+# info :   CACHE https://api.nuget.org/v3-vulnerabilities/2024.10.11.23.31.19/2024.10.11.23.31.19/vulnerability.update.json
+# info : Package 'Moq' is compatible with all the specified frameworks in project 'D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch07\SportsSln\SportsStore.Tests\SportsStore.Tests.csproj'.
+# info : PackageReference for package 'Moq' version '4.18.4' added to file 'D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch07\SportsSln\SportsStore.Tests\SportsStore.Tests.csproj'.
+# info : Writing assets file to disk. Path: D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch07\SportsSln\SportsStore.Tests\obj\project.assets.json
+# log  : Restored D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch07\SportsSln\SportsStore.Tests\SportsStore.Tests.csproj (in 435 ms).
