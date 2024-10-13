697e6f8f39ecc473a5cc5687619d3d283d28e986
David Stevenson <drstevenson1958@yahoo.com>
2024-10-13 12:22:43 -0400

    Listing 7.21 Creating the database migration
    dotnet ef migrations add Initial

---
+++ ch07/Listing_7-21.ps1
@@ -0,0 +1,9 @@
+# Listing 7.21 Creating the database migration
+
+dotnet ef migrations add Initial
+
+# PS D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch07\SportsSln\SportsStore> ..\..\Listing_7-21.ps1
+# Build started...
+# Build succeeded.
+# Done. To undo this action, use 'ef migrations remove'
+# PS D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch07\SportsSln\SportsStore> 
