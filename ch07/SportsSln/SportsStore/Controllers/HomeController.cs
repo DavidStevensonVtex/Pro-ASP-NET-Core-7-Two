e13fb5b1ae034518b4b350f18ea902974432d778
David Stevenson <drstevenson1958@yahoo.com>
2024-10-13 11:23:02 -0400

    Listing 7.9 The contents of the HomeController.cs file in the SportsStore/Controllers folder

---
+++ ch07/SportsSln/SportsStore/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿// Listing 7.9 The contents of the HomeController.cs file in the SportsStore/Controllers folder
+
+namespace SportsStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
