eb4d38c067ed0ba32bf519d990f85babb35afb07
David Stevenson <drstevenson1958@yahoo.com>
2024-10-13 15:18:23 -0400

    Unit test: Creating page links

--- ch07/SportsSln/SportsStore.Tests/GlobalUsings.cs
+++ ch07/SportsSln/SportsStore.Tests/GlobalUsings.cs
@@ -1,4 +1,6 @@
 ﻿global using SportsStore.Models;
+global using SportsStore.Models.ViewModels;
 global using SportsStore.Controllers;
+global using SportsStore.Infrastructure;
 global using Microsoft.AspNetCore.Mvc;
 global using Moq;
