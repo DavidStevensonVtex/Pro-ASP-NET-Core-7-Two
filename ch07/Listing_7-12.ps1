249091a5dab1e397d406ffaf06bf92ff593ba012
David Stevenson <drstevenson1958@yahoo.com>
2024-10-13 11:32:14 -0400

    Listing 7.12 Running the example application
    dotnet run

---
+++ ch07/Listing_7-12.ps1
@@ -0,0 +1,16 @@
+# Listing 7.12 Running the example application
+
+dotnet run
+
+# PS D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch07\SportsSln\SportsStore> ..\..\Listing_7-12.ps1
+# Building...
+# warn: Microsoft.AspNetCore.StaticFiles.StaticFileMiddleware[16]
+#       The WebRootPath was not found: D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch07\SportsSln\SportsStore\wwwroot. Static files may be unavailable.
+# info: Microsoft.Hosting.Lifetime[14]
+#       Now listening on: http://localhost:5000
+# info: Microsoft.Hosting.Lifetime[0]
+#       Application started. Press Ctrl+C to shut down.
+# info: Microsoft.Hosting.Lifetime[0]
+#       Hosting environment: Development
+# info: Microsoft.Hosting.Lifetime[0]
+#       Content root path: D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch07\SportsSln\SportsStore
