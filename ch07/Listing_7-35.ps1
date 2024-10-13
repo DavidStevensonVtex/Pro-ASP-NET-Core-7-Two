366bdb28c148d1cb857a802f927ca1045f24eb2b
David Stevenson <drstevenson1958@yahoo.com>
2024-10-13 15:59:06 -0400

    Listing 7.35 Installing the LibMan tool package

---
+++ ch07/Listing_7-35.ps1
@@ -0,0 +1,8 @@
+# Listing 7.35 Installing the LibMan tool package
+
+dotnet tool uninstall --global Microsoft.Web.LibraryManager.Cli 
+dotnet tool install --global Microsoft.Web.LibraryManager.Cli  --version 2.1.175
+
+# Tool 'microsoft.web.librarymanager.cli' (version '2.1.175') was successfully uninstalled.
+# You can invoke the tool using the following command: libman
+# Tool 'microsoft.web.librarymanager.cli' (version '2.1.175') was successfully installed.
