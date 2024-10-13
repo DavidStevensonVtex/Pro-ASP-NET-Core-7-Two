bed9feeded5c8b81cc132fbe0939f5f14173c3c1
David Stevenson <drstevenson1958@yahoo.com>
2024-10-13 11:36:40 -0400

    Listing 7.13 Adding the Entity Framework Core packages to the SportsStore project

---
+++ ch07/Listing_7-13.ps1
@@ -0,0 +1,12 @@
+# Listing 7.13 Adding the Entity Framework Core packages to the SportsStore project
+
+dotnet add package Microsoft.EntityFrameworkCore.Design --version 8.0.10
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer --version 8.0.10
+
+# PS D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch07\SportsSln\SportsStore> ..\..\Listing_7-13.ps1
+#   Determining projects to restore...
+#   Writing C:\Users\drste\AppData\Local\Temp\tmpdurd53.tmp
+# info : X.509 certificate chain validation will use the default trust store selected by .NET for code signing.
+# info : X.509 certificate chain validation will use the default trust store selected by .NET for timestamping.
+# info : Adding PackageReference for package 'Microsoft.EntityFrameworkCore.Design' into project 'D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch07\SportsSln\SportsStore\SportsStore.csproj'.
+# ...
