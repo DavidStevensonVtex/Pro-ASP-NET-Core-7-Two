e097c77b897bbd37906f75b29ca3a70dcd147e6b
David Stevenson <drsgis@rit.edu>
2024-10-16 14:35:01 -0400

    Listing 12.3 Starting the example application

---
+++ ch12/Listing_12-03.ps1
@@ -0,0 +1,14 @@
+# Listing 12.3 Starting the example application
+
+cd Platform
+dotnet run
+
+# PS C:\Users\drsgis\drs\Pro-ASP-NET-Core-7-Two\ch12> .\Listing_12-03.ps1
+# Building...
+# info: Microsoft.Hosting.Lifetime[14]
+#       Now listening on: http://localhost:5000
+# info: Microsoft.Hosting.Lifetime[0]
+#       Application started. Press Ctrl+C to shut down.
+# info: Microsoft.Hosting.Lifetime[0]
+#       Hosting environment: Development
+# info: Microsoft.Hosting.Lifetime[0]
