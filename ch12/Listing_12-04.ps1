f6aac723ab813dbbe24875be17c2e2f5c39e9bcf
David Stevenson <drsgis@rit.edu>
2024-10-16 14:46:44 -0400

    Listing 12.4 Sending an HTTP Request
    (Invoke-WebRequest http://localhost:5000).RawContent

---
+++ ch12/Listing_12-04.ps1
@@ -0,0 +1,11 @@
+# Listing 12.4 Sending an HTTP Request
+
+(Invoke-WebRequest http://localhost:5000).RawContent
+
+# HTTP/1.1 200 OK
+# Date: Wed, 16 Oct 2024 18:46:02 GMT
+# Server: Kestrel
+# Transfer-Encoding: chunked
+# Content-Type: text/plain; charset=utf-8
+
+# Hello World!
