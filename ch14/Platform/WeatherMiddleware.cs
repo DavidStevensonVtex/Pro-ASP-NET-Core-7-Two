7b3750875edbd690842ba90083e59571387e5db4
David Stevenson <drsgis@rit.edu>
2024-10-18 13:26:51 -0400

    Listing 14.3 The contents of the WeatherMiddleware.cs file in the Platform folder

---
+++ ch14/Platform/WeatherMiddleware.cs
@@ -0,0 +1,23 @@
+﻿// Listing 14.3 The contents of the WeatherMiddleware.cs file in the Platform folder
+
+namespace Platform
+{
+	public class WeatherMiddleware
+	{
+		private RequestDelegate next;
+
+		public WeatherMiddleware(RequestDelegate nextDelegate) { this.next = nextDelegate; }
+
+		public async Task Invoke (HttpContext context)
+		{
+			if (context.Request.Path == "/middleware/class")
+			{
+				await context.Response.WriteAsync("Middleware Class: It is raining in London");
+			}
+			else
+			{
+				await next(context);
+			}
+		}
+	}
+}
