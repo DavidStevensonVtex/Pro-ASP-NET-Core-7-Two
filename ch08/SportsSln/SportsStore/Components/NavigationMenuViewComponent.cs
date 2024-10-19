6d5ad85bbcad40fcdc6bf60f6ea29f514e52fbc5
David Stevenson <drstevenson1958@yahoo.com>
2024-10-19 14:50:55 -0400

    Listing 8.6 The contents of the NavigationMenuViewComponent.cs file in the SportsStore/Components folder

---
+++ ch08/SportsSln/SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,12 @@
+﻿// Listing 8.6 The contents of the NavigationMenuViewComponent.cs file in the SportsStore/Components folder
+
+namespace SportsStore.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        public string Invoke()
+        {
+            return "Hello from the Nav View Component";
+        }
+    }
+}
