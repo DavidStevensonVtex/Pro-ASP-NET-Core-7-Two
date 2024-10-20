43aec52be13dfac096995c830ab2c028d928b6c8
David Stevenson <drstevenson1958@yahoo.com>
2024-10-20 14:33:58 -0400

    Listing 9.14 The Checkout.cshtml file in the SportsStore/Views/Order folder

--- ch09/SportsSln/SportsStore/Models/Order.cs
+++ ch09/SportsSln/SportsStore/Models/Order.cs
@@ -20,7 +20,10 @@ namespace SportsStore.Models
         public string? Line2 { get; set; }
         public string? Line3 { get; set; }
 
-        [Required(ErrorMessage = "Please enter a state naem")]
+        [Required(ErrorMessage = "Please enter a city name")]
+        public string? City { get; set; }
+
+        [Required(ErrorMessage = "Please enter a state name")]
         public string? State { get; set; }
 
         public string? Zip { get; set; }
