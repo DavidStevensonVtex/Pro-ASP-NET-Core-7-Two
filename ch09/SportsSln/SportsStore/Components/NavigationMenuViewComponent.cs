23c89f956e9c8b474cdda3e3e2b1f64c77602763
David Stevenson <drstevenson1958@yahoo.com>
2024-10-20 07:35:48 -0400

    Chapter 9: SportsStore: Completing the cart

---
+++ ch09/SportsSln/SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,23 @@
+﻿// Listing 8.10 Passing the selected category in the NavigationMenuViewComponent.cs file in the SportsStore/Components folder
+
+namespace SportsStore.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private IStoreRepository repository;
+
+        public NavigationMenuViewComponent(IStoreRepository repo)
+        {
+            repository = repo;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedCategory = RouteData?.Values["category"];
+            return View(repository.Products
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x));
+        }
+    }
+}
