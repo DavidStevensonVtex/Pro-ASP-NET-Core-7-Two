9525f204d595211209cd079446b0331a4601cce2
David Stevenson <drsgis@rit.edu>
2024-10-21 14:46:39 -0400

    Listing 15.32 Installing LibMan
    dotnet tool uninstall --global Microsoft.Web.LibraryManager.Cli
    dotnet tool install --global Microsoft.Web.LibraryManager.Cli --version 2.1.175

---
+++ ch15/Listing_15-32.ps1
@@ -0,0 +1,9 @@
+# Listing 15.32 Installing LibMan
+
+dotnet tool uninstall --global Microsoft.Web.LibraryManager.Cli 
+dotnet tool install --global Microsoft.Web.LibraryManager.Cli --version 2.1.175
+
+# PS C:\Users\drsgis\drs\Pro-ASP-NET-Core-7-Two\ch15\Platform> ..\Listing_15-32.ps1
+# Tool 'microsoft.web.librarymanager.cli' (version '2.1.175') was successfully uninstalled.
+# You can invoke the tool using the following command: libman
+# Tool 'microsoft.web.librarymanager.cli' (version '2.1.175') was successfully installed.
