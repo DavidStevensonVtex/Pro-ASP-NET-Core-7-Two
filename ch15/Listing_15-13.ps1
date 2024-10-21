b2e4051e8311359ceca70ad84683242ccb05232e
David Stevenson <drsgis@rit.edu>
2024-10-21 11:19:25 -0400

    Listing 15.13 Initializing user secrets
    dotnet user-secrets init

---
+++ ch15/Listing_15-13.ps1
@@ -0,0 +1,6 @@
+# Listing 15.13 Initializing user secrets
+
+dotnet user-secrets init
+
+# PS C:\Users\drsgis\drs\Pro-ASP-NET-Core-7-Two\ch15\Platform> ..\Listing_15-13.ps1
+# Set UserSecretsId to '3e37d132-8fc7-4ca9-85b2-647ae7b64b95' for MSBuild project 'C:\Users\drsgis\drs\Pro-ASP-NET-Core-7-Two\ch15\Platform\Platform.csproj'.
