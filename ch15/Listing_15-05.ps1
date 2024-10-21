71cb7c9ab691d77e3b985ddc73e6bb002d7deb4b
David Stevenson <drsgis@rit.edu>
2024-10-21 10:15:50 -0400

    Listing 15.5 Starting the ASP.NET Core runtime
    dotnet run

---
+++ ch15/Listing_15-05.ps1
@@ -0,0 +1,14 @@
+# Listing 15.5 Starting the ASP.NET Core runtime
+
+dotnet run
+
+# PS C:\Users\drsgis\drs\Pro-ASP-NET-Core-7-Two\ch15\Platform> ..\Listing_15-05.ps1
+# Building...
+# info: Microsoft.Hosting.Lifetime[14]
+#       Now listening on: http://localhost:5000
+# info: Microsoft.Hosting.Lifetime[0]
+#       Application started. Press Ctrl+C to shut down.
+# info: Microsoft.Hosting.Lifetime[0]
+#       Hosting environment: Development
+# info: Microsoft.Hosting.Lifetime[0]
+#       Content root path: C:\Users\drsgis\drs\Pro-ASP-NET-Core-7-Two\ch15\Platform
