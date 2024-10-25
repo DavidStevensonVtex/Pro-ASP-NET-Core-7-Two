a86809dd1e596e6bbf3f3832c948d2de6978258d
David Stevenson <drstevenson1958@yahoo.com>
2024-10-25 10:51:26 -0400

    Listing 10.9 Creating a new migration for Shipped Orders (new field Shipped in the Order class

---
+++ ch10/Listing_10-09.ps1
@@ -0,0 +1,8 @@
+# Listing 10.9 Creating a new migration
+
+dotnet ef migrations add ShippedOrders
+
+# PS D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch10\SportsSln\SportsStore> ..\..\Listing_10-09.ps1
+# Build started...
+# Build succeeded.
+# Done. To undo this action, use 'ef migrations remove'
