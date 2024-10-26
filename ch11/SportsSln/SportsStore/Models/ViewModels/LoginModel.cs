c480edb03a2ed8a467c114285cd93e8e94488f70
David Stevenson <drstevenson1958@yahoo.com>
2024-10-26 10:49:34 -0400

    Listing 11.12 The LoginModel.cs file in the SportsStore/Models/ViewModels folder

---
+++ ch11/SportsSln/SportsStore/Models/ViewModels/LoginModel.cs
@@ -0,0 +1,11 @@
+﻿// Listing 11.12 The LoginModel.cs file in the SportsStore/Models/ViewModels folder
+
+namespace SportsStore.Models.ViewModels
+{
+    public class LoginModel
+    {
+        public required string Name { get; set; }
+        public required string Password { get; set; }
+        public string ReturnUrl { get; set; } = "/";
+    }
+}
