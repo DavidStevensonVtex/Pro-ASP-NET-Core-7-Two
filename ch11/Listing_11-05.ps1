421ab00e1da2efe28a1881faf3a588d730341703
David Stevenson <drstevenson1958@yahoo.com>
2024-10-26 10:05:53 -0400

    Listing 11.5 Creating the identity migration

---
+++ ch11/Listing_11-05.ps1
@@ -0,0 +1,8 @@
+# Listing 11.5 Creating the identity migration
+
+dotnet ef migrations add Initial --context AppIdentityDbContext
+
+# PS D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch11\SportsSln\SportsStore> ..\..\Listing_11-05.ps1
+# Build started...
+# Build succeeded.
+# Done. To undo this action, use 'ef migrations remove'
