5901f05803762d2d4ebb5797a6a225c487a54025
David Stevenson <drstevenson1958@yahoo.com>
2024-10-27 11:12:27 -0400

    Listing 17.21 Testing the Entity Framework Core Global Tool
    dotnet ef --help

---
+++ ch17/Listing_17-21.ps1
@@ -0,0 +1,21 @@
+# Listing 17.21 Testing the Entity Framework Core Global Tool
+
+dotnet ef --help
+
+# Entity Framework Core .NET Command-line Tools 8.0.10
+
+# Usage: dotnet ef [options] [command]
+
+# Options:
+#   --version        Show version information
+#   -h|--help        Show help information
+#   -v|--verbose     Show verbose output.
+#   --no-color       Don't colorize output.
+#   --prefix-output  Prefix output with level.
+
+# Commands:
+#   database    Commands to manage the database.
+#   dbcontext   Commands to manage DbContext types.
+#   migrations  Commands to manage migrations.
+
+# Use "dotnet ef [command] --help" for more information about a command.
