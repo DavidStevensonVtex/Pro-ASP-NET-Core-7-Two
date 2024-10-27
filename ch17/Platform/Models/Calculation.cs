72390c85897406040954ab8b99d45bda2c63e687
David Stevenson <drstevenson1958@yahoo.com>
2024-10-27 11:20:00 -0400

    Listing 17.23 The contents of the Calculation.cs file in the Platform/Models folder

---
+++ ch17/Platform/Models/Calculation.cs
@@ -0,0 +1,11 @@
+﻿// Listing 17.23 The contents of the Calculation.cs file in the Platform/Models folder
+
+namespace Platform.Models
+{
+    public class Calculation
+    {
+        public long Id { get; set; }
+        public int Count { get; set; }
+        public long Result { get; set; }
+    }
+}
