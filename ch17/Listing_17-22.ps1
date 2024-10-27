dc82813a86ea5924c34a9cef55bcf4a0e6b2f55d
David Stevenson <drstevenson1958@yahoo.com>
2024-10-27 11:15:15 -0400

    Listing 17.22 Adding Entity Framework Core Packages to the Project
    dotnet add package Microsoft.EntityFrameworkCore.Design --version 8.0.10
    dotnet add package Microsoft.EntityFrameworkCore.SqlServer --version 8.0.10

---
+++ ch17/Listing_17-22.ps1
@@ -0,0 +1,5 @@
+# Listing 17.22 Adding Entity Framework Core Packages to the Project
+
+dotnet add package Microsoft.EntityFrameworkCore.Design --version 8.0.10
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer --version 8.0.10
+
