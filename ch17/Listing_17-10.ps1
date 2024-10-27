45ee818098ab0bb0cff34574b77f208561a12ce1
David Stevenson <drstevenson1958@yahoo.com>
2024-10-27 05:20:57 -0400

    Listing 17.10 Creating the cache database table

---
+++ ch17/Listing_17-10.ps1
@@ -0,0 +1,5 @@
+# Listing 17.10 Creating the cache database table
+
+dotnet sql-cache create "Server=localhost\SQL_2022;Database=CacheDB;MultipleActiveResultSets=true;Integrated Security=True;TrustServerCertificate=true;" dbo DataCache
+
+# Table and index were created successfully
