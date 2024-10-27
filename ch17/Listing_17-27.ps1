de4b946c2a5313b97c470fa24d0f7c1e667a298c
David Stevenson <drstevenson1958@yahoo.com>
2024-10-27 11:43:41 -0400

    Listing 17.27 Creating a migration
    dotnet ef migrations add Initial

---
+++ ch17/Listing_17-27.ps1
@@ -0,0 +1,8 @@
+# Listing 17.27 Creating a migration
+
+dotnet ef migrations add Initial
+
+# PS D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch17\Platform> ..\Listing_17-27.ps1
+# Build started...
+# Build succeeded.
+# Done. To undo this action, use 'ef migrations remove'
