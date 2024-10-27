7f40a41e0d2449fd130fc5f46c747204732835fe
David Stevenson <drstevenson1958@yahoo.com>
2024-10-27 05:13:40 -0400

    Listing 17.9 Installing the SQL cache package
    dotnet tool uninstall --global dotnet-sql-cache
    dotnet tool install --global dotnet-sql-cache --version 8.0.10

---
+++ ch17/Listing_17-09.ps1
@@ -0,0 +1,8 @@
+# Listing 17.9 Installing the SQL cache package
+
+dotnet tool uninstall --global dotnet-sql-cache
+dotnet tool install --global dotnet-sql-cache --version 8.0.10
+
+# Tool 'dotnet-sql-cache' (version '7.0.10') was successfully uninstalled.
+# You can invoke the tool using the following command: dotnet-sql-cache
+# Tool 'dotnet-sql-cache' (version '8.0.10') was successfully installed.
