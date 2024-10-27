d02334e01ca31be086751ddcc221dbe6a69da7ad
David Stevenson <drstevenson1958@yahoo.com>
2024-10-27 11:10:06 -0400

    Listing 17.20 Installing the Entity Framework Core Global Tool Package

---
+++ ch17/Listing_17-20.ps1
@@ -0,0 +1,8 @@
+# Listing 17.20 Installing the Entity Framework Core Global Tool Package
+
+dotnet tool uninstall --global dotnet-ef
+dotnet tool install --global dotnet-ef --version 8.0.10
+
+# Tool 'dotnet-ef' (version '8.0.10') was successfully uninstalled.
+# You can invoke the tool using the following command: dotnet-ef 
+# Tool 'dotnet-ef' (version '8.0.10') was successfully installed.
