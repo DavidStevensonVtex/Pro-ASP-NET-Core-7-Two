e2cedfcae40284b77d9199829e65c81ea8f80381
David Stevenson <drstevenson1958@yahoo.com>
2024-10-29 09:18:18 -0400

    Listing 18.3 Adding packages to the project

---
+++ ch18/Listing_18-03.ps1
@@ -0,0 +1,8 @@
+# Listing 18.3 Adding packages to the project
+
+dotnet add package Microsoft.EntityFrameworkCore.Design --version 8.0.10
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer --version 8.0.10
+
+# PS D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch18\WebApp> ..\Listing_18-03.ps1
+#   Determining projects to restore...
+#   ...
