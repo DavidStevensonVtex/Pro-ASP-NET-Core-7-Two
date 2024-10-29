7c87761079aca458d2f29bef45c654ec6e038a32
David Stevenson <drstevenson1958@yahoo.com>
2024-10-29 15:44:58 -0400

    Listing 19.22 Testing redirection
    Invoke-RestMethod http://localhost:5000/api/products/redirect

---
+++ ch19/Listing_19-22.ps1
@@ -0,0 +1,13 @@
+# Listing 19.22 Testing redirection
+
+Invoke-RestMethod http://localhost:5000/api/products/redirect 
+
+# PS D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch19\WebApp> ..\Listing_19-22.ps1
+
+# productId  : 1
+# name       : Green Kayak
+# price      : 275
+# categoryId : 2
+# category   : 
+# supplierId : 2
+# supplier   : 
