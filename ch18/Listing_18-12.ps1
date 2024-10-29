f4bd0a2e18b48f87ce0c0057b2db538f9cd9cc50
David Stevenson <drstevenson1958@yahoo.com>
2024-10-29 09:54:45 -0400

    Listing 18.12 Creating an Entity Framework Core Migration
    dotnet ef migrations add Initial

---
+++ ch18/Listing_18-12.ps1
@@ -0,0 +1,11 @@
+# Listing 18.12 Creating an Entity Framework Core Migration
+
+dotnet ef migrations add Initial
+
+# PS D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch18\WebApp> ..\Listing_18-12.ps1
+# Build started...
+# Build succeeded.                                                                                                                                    Ape-7
+# warn: Microsoft.EntityFrameworkCore.Model.Validation[10400]
+#       Sensitive data logging is enabled. Log entries and exception messages may include sensitive application data; this mode should only be enabled  \ch during development.
+# Done. To undo this action, use 'ef migrations remove'
+# PS D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch18\WebApp> 
