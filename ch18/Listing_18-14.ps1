8666edbbe9f700259b27754f17529074b80356aa
David Stevenson <drstevenson1958@yahoo.com>
2024-10-29 10:00:53 -0400

    Listing 18.14 Installing the LibMan tool package

---
+++ ch18/Listing_18-14.ps1
@@ -0,0 +1,9 @@
+# Listing 18.14 Installing the LibMan tool package
+
+dotnet tool uninstall --global Microsoft.Web.LibraryManager.Cli 
+dotnet tool install --global Microsoft.Web.LibraryManager.Cli --version 2.1.175
+
+# PS D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch18\WebApp> ..\Listing_18-14.ps1
+# Tool 'microsoft.web.librarymanager.cli' (version '2.1.175') was successfully uninstalled.
+# You can invoke the tool using the following command: libman
+# Tool 'microsoft.web.librarymanager.cli' (version '2.1.175') was successfully installed.
