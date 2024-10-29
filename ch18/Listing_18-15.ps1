81a1b8569c839d269d65a611525f1285eb15a0d9
David Stevenson <drstevenson1958@yahoo.com>
2024-10-29 10:03:22 -0400

    Listing 18.15 Installing the Bootstrap CSS framework
    libman init -p cdnjs
    libman install bootstrap@5.2.3 -d wwwroot/lib/boostrap

---
+++ ch18/Listing_18-15.ps1
@@ -0,0 +1,9 @@
+# Listing 18.15 Installing the Bootstrap CSS framework
+
+libman init -p cdnjs 
+libman install bootstrap@5.2.3 -d wwwroot/lib/boostrap 
+
+# PS D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch18\WebApp> ..\Listing_18-15.ps1
+# wwwroot/lib/boostrap/css/bootstrap-grid.css written to disk
+# ...
+# Installed library "bootstrap@5.2.3" to "wwwroot/lib/boostrap"
