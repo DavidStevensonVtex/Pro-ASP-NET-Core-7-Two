1adcd9fc92dd9aac07795eb088c621484b68e406
David Stevenson <drstevenson1958@yahoo.com>
2024-10-30 11:03:40 -0400

    Listing 20.14 Enabling XML formatting in the Program.cs file in the WebApp folder

--- ch20/Listing_20-13.ps1
+++ ch20/Listing_20-13.ps1
@@ -9,3 +9,11 @@ Invoke-WebRequest http://localhost:5000/api/content/object `
 # Content-Type                    Content
 # ------------                    -------
 # application/json; charset=utf-8 {"productId":1,"name":"Kayak","price":275.00,"categoryId":1,"supplierId":1}
+
+# After implementing: Listing 20.14 Enabling XML formatting in the Program.cs file in the WebApp folder
+
+# PS D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch20\WebApp> ..\Listing_20-13.ps1
+
+# Content-Type                   Content
+# ------------                   -------
+# application/xml; charset=utf-8 <Product xmlns:i="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://schemas.datacontract.org/2004/07/WebA…
