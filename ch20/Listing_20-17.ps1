f373642157cffaa075c0d5803da761f78941ae6b
David Stevenson <drstevenson1958@yahoo.com>
2024-10-30 11:27:43 -0400

    Listing 20.17 Requesting a PNG response - After implementing: Listing 20.18 Configuring content negotiaton in the Program.cs file in the WebApp folder

--- ch20/Listing_20-17.ps1
+++ ch20/Listing_20-17.ps1
@@ -9,3 +9,12 @@ Invoke-WebRequest http://localhost:5000/api/content/object `
 # Content-Type                    Content
 # ------------                    -------
 # application/json; charset=utf-8 {"name":"Kayak","price":275.00,"categoryId":1,"supplierId":1}
+
+# After implementing: Listing 20.18 Configuring content negotiaton in the Program.cs file in the WebApp folder
+
+# PS D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch20\WebApp> ..\Listing_20-17.ps1
+# Invoke-WebRequest: D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch20\Listing_20-17.ps1:3:1
+# Line |
+#    3 |  Invoke-WebRequest http://localhost:5000/api/content/object `
+#      |  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#      | Response status code does not indicate success: 406 (Not Acceptable).
