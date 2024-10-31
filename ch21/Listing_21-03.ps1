02651e9326c6e683f3b2e0f2722a7fcd65430657
David Stevenson <drstevenson1958@yahoo.com>
2024-10-31 09:54:21 -0400

    Listing 21.3 Running the example application
    dotnet watch

---
+++ ch21/Listing_21-03.ps1
@@ -0,0 +1,12 @@
+# Listing 21.3 Running the example application
+
+dotnet watch
+
+# PS D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch21\WebApp> ..\Listing_21-03.ps1
+# dotnet watch 🔥 Hot reload enabled. For a list of supported edits, see https://aka.ms/dotnet/hot-reload.
+#   💡 Press "Ctrl + R" to restart.
+# dotnet watch 🔧 Building...
+#   Determining projects to restore...
+#   All projects are up-to-date for restore.
+#   WebApp -> D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch21\WebApp\bin\Debug\net8.0\WebApp.dll
+# dotnet watch 🚀 Started
