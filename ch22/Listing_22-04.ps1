a3c1490f897827df916254c1e81d156307862075
David Stevenson <drstevenson1958@yahoo.com>
2024-11-01 11:21:11 -0400

    Listing 22.4 Running the example application
    dotnet watch

---
+++ ch22/Listing_22-04.ps1
@@ -0,0 +1,21 @@
+# Listing 22.4 Running the example application
+
+dotnet watch
+
+# PS D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch22\WebApp> ..\Listing_22-04.ps1
+# dotnet watch 🔥 Hot reload enabled. For a list of supported edits, see https://aka.ms/dotnet/hot-reload.
+#   💡 Press "Ctrl + R" to restart.
+# dotnet watch 🔧 Building...
+#   Determining projects to restore...
+#   All projects are up-to-date for restore.
+#   WebApp -> D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch22\WebApp\bin\Debug\net8.0\WebApp.dll
+# dotnet watch 🚀 Started
+# ...
+# info: Microsoft.Hosting.Lifetime[14]
+#       Now listening on: http://localhost:5000
+# info: Microsoft.Hosting.Lifetime[0]
+#       Application started. Press Ctrl+C to shut down.
+# info: Microsoft.Hosting.Lifetime[0]
+#       Hosting environment: Development
+# info: Microsoft.Hosting.Lifetime[0]
+#       Content root path: D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch22\WebApp
