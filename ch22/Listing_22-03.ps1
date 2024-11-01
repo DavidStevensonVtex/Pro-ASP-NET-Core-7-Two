d438414431e6aee504cbe838ecda6a8ebc5ee0d3
David Stevenson <drstevenson1958@yahoo.com>
2024-11-01 11:19:10 -0400

    Listing 22.3 Dropping the database
    dotnet ef database drop --force

---
+++ ch22/Listing_22-03.ps1
@@ -0,0 +1,24 @@
+# Listing 22.3 Dropping the database
+
+dotnet ef database drop --force
+
+# PS D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch22\WebApp> ..\Listing_22-03.ps1
+# Build started...
+# Build succeeded.
+# warn: Microsoft.EntityFrameworkCore.Model.Validation[10400]
+#       Sensitive data logging is enabled. Log entries and exception messages may include sensitive application data; this mode should only be enabled 
+# during development.
+# Dropping database 'Products_Core_7' on server 'localhost\SQL_2022'.
+# info: Microsoft.EntityFrameworkCore.Database.Command[20101]
+#       Executed DbCommand (19ms) [Parameters=[], CommandType='Text', CommandTimeout='30']
+#       SELECT 1
+# info: Microsoft.EntityFrameworkCore.Database.Command[20101]
+#       Executed DbCommand (221ms) [Parameters=[], CommandType='Text', CommandTimeout='60']
+#       IF SERVERPROPERTY('EngineEdition') <> 5
+#       BEGIN
+#           ALTER DATABASE [Products_Core_7] SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+#       END;
+# info: Microsoft.EntityFrameworkCore.Database.Command[20101]
+#       Executed DbCommand (352ms) [Parameters=[], CommandType='Text', CommandTimeout='60']
+#       DROP DATABASE [Products_Core_7];
+# Successfully dropped database 'Products_Core_7'.
