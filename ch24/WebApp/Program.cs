ee8f304cb56924577c918567ded1c8d380e786b7
David Stevenson <drstevenson1958@yahoo.com>
2024-11-03 12:22:52 -0500

    Chapter 24: Using View components

---
+++ ch24/WebApp/Program.cs
@@ -0,0 +1,36 @@
+// Listing 23.8 Adding Razor Page routes in the Program.cs file in the WebApp folder
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DataContext> (opts =>
+{
+    opts.UseSqlServer(builder.Configuration["ConnectionStrings:ProductConnection"]);
+    opts.EnableSensitiveDataLogging(true);
+});
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.Cookie.IsEssential = true;
+});
+
+builder.Services.Configure<RazorPagesOptions>(opts =>
+{
+    opts.Conventions.AddPageRoute("/Index", "/extra/page/{id:long?}");
+});
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseSession();
+app.MapControllers();
+app.MapDefaultControllerRoute();
+app.MapRazorPages();
+
+var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<DataContext>();
+SeedData.SeedDatabase(context);
+
+app.Run();
