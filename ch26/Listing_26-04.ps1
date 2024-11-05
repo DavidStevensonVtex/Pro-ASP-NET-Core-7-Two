a30e7ed36f104d19df7af7f3986e841ab17cad3f
David Stevenson <drstevenson1958@yahoo.com>
2024-11-05 09:03:23 -0500

    Listing 26.4 Installing a package

---
+++ ch26/Listing_26-04.ps1
@@ -0,0 +1,8 @@
+# Listing 26.4 Installing a package
+
+libman install jquery@3.6.3 -d wwwroot/lib/jquery
+
+# Instead use CDN
+
+# https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js
+# https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.js
