64319638f0e2f99c4b8d46df899cb44b0e615af7
David Stevenson <drstevenson1958@yahoo.com>
2024-11-10 15:24:50 -0500

    Chapter 30: Using Filters - Listing 30.1 Removing files from the project

---
+++ ch30/Listing_30-01.ps1
@@ -0,0 +1,3 @@
+# Listing 30.1 Removing files from the project
+
+Remove-Item -Path Controllers,Views,Pages -Recurse -Exclude _*,Shared
---
+++ ch30/WebApp/Models/Product.cs
@@ -0,0 +1,33 @@
+﻿// Listing 29.26 Using the Remote validation attribute in the Product.cs file in the Models folder
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+using WebApp.Validation;
+
+namespace WebApp.Models
+{
+    [PhraseAndPrice(Phrase = "Small", Price = "100")]
+    public class Product
+    {
+        public long ProductId { get; set; }
+
+        [Required(ErrorMessage = "Please enter a name")]
+        public required string Name { get; set; }
+
+        [Range(1, 999999, ErrorMessage = "Please enter a positive price")]
+        [Column(TypeName = "decimal(8, 2)")]
+        public decimal Price { get; set; }
+
+        [Validation.PrimaryKey(ContextType = typeof(DataContext), DataType = typeof(Category))]
+        [Remote("CategoryKey", "Validation", ErrorMessage = "Enter an existing key" )]
+        public long CategoryId {  get ; set; }
+        public Category? Category {  get; set; }
+
+        [Validation.PrimaryKey(ContextType = typeof(DataContext), DataType = typeof(Supplier))]
+        [Remote("SupplierKey", "Validation", ErrorMessage = "Enter an existing key")]
+        public long SupplierId { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public Supplier? Supplier { get; set; }
+    }
+}
