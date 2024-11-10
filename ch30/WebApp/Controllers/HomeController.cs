00c079b2bf27495c58f4b08de2b4280d455fb4cb
David Stevenson <drstevenson1958@yahoo.com>
2024-11-10 15:29:11 -0500

    Listing 30.2 The contents of the HomeController.cs file in the Controllers folder

---
+++ ch30/WebApp/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿// Listing 30.2 The contents of the HomeController.cs file in the Controllers folder
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Message", "This is the Index action on the Home Controller");
+        }
+    }
+}
