69d986443b1822279cae4bc994b38346c86c9ab6
David Stevenson <drstevenson1958@yahoo.com>
2024-11-13 14:01:55 -0500

    Listing 32.3 Adding packages to the project
    dotnet add package Microsoft.EntityFrameworkCore.Design --version 8.0.11
    dotnet add package Microsoft.EntityFrameworkCore.SqlServer --version 8.0.11

---
+++ ch32/Listing_32-03.ps1
@@ -0,0 +1,4 @@
+# Listing 32.3 Adding packages to the project
+
+dotnet add package Microsoft.EntityFrameworkCore.Design --version 8.0.11
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer --version 8.0.11
