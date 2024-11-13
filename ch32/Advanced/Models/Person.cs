50dfd96754307deab5802062ca7c2696d7ac6dc2
David Stevenson <drstevenson1958@yahoo.com>
2024-11-13 14:16:12 -0500

    Listing 32.5 the contents of hte Person.cs file in the Models folder

--- ch32/Advanced/Models/Person.cs
+++ ch32/Advanced/Models/Person.cs
@@ -5,8 +5,8 @@ namespace Advanced.Models
     public class Person
     {
         public long PersonId { get; set; }
-        public string FirstName { get; set; }
-        public string Surname {  get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string Surname { get; set; } = string.Empty;
         public long DepartmentId { get; set; }
         public long LocationId { get; set; }
 
