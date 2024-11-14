8a3c5ae985f2ba82e9fc785fe0b70495d28d940f
David Stevenson <drstevenson1958@yahoo.com>
2024-11-14 14:02:31 -0500

    Listing 33.22 The contents of the Split.razor.cs file in the Balzor folder

---
+++ ch33/Advanced/Blazor/Split.razor.cs
@@ -0,0 +1,17 @@
+﻿// Listing 33.22 The contents of the Split.razor.cs file in the Balzor folder
+
+using Advanced.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace Advanced.Blazor {
+
+    public partial class Split {
+
+        [Inject]
+        public DataContext? Context { get; set; }
+
+        public IEnumerable<string> Names =>
+            Context?.People.Select(p => p.Firstname)
+                ?? Enumerable.Empty<string>();
+    }
+}
