e783f265d0480b193eba0a1f2049f51b42240d53
David Stevenson <drstevenson1958@yahoo.com>
2024-11-17 14:29:48 -0500

    Listing 35.25 The contents of the interop.js file in the wwwroot folder

--- ch35/Advanced/wwwroot/interop.js
+++ ch35/Advanced/wwwroot/interop.js
@@ -3,7 +3,7 @@
 function addTableRows(colCount) {
     let elem = document.querySelector("tbody");
     let row = document.createElement("tr");
-    elem.append(row, elem);
+    elem.append(row);
     for (let i = 0; i < colCount; i++) {
         let cell = document.createElement("td");
         cell.innerText = "New Elements"
