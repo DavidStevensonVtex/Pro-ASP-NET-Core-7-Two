fddb1fc590d20ce205439ca4a86eae3765603201
David Stevenson <drstevenson1958@yahoo.com>
2024-11-19 09:36:30 -0500

    Listing 37.6 Preparing the advanced project

---
+++ ch37/Listing_37-06.ps1
@@ -0,0 +1,4 @@
+# Listing 37.6 Preparing the advanced project
+
+dotnet add reference ../DataModel ../BlazorWebAssembly
+dotnet add package Microsoft.AspNetCore.Components.WebAssembly.Server --version 8.0.11
