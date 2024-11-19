29410d977c415e50fdd0543441746ffcb94c530d
David Stevenson <drstevenson1958@yahoo.com>
2024-11-19 09:38:03 -0500

    Listing 37.7 Adding solution references

---
+++ ch37/Listing_37-07.ps1
@@ -0,0 +1,7 @@
+# Listing 37.7 Adding solution references
+
+dotnet sln add ../DataModel ../BlazorWebAssembly
+
+# PS D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch37\Advanced> ..\Listing_37-07.ps1
+# Project `..\DataModel\DataModel.csproj` added to the solution.
+# Project `..\BlazorWebAssembly\BlazorWebAssembly.csproj` added to the solution.
