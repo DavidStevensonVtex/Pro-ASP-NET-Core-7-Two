e186ee719b91244f07df367df5d1782c83da9a17
David Stevenson <drstevenson1958@yahoo.com>
2024-11-19 09:33:37 -0500

    Listing 37.5 Creating the Blazor WebAssembly project

---
+++ ch37/Listing_37-05.ps1
@@ -0,0 +1,17 @@
+# Listing 37.5 Creating the Blazor WebAssembly project
+
+dotnet new blazorwasm -o ../BlazorWebAssembly -f net8.0
+dotnet add ../BlazorWebAssembly reference ../DataModel
+
+# PS D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch37\Advanced> ..\Listing_37-05.ps1
+# The template "Blazor WebAssembly Standalone App" was created successfully.
+# This template contains technologies from parties other than Microsoft, see https://aka.ms/aspnetcore/8.0-third-party-notices for details.        
+
+# Processing post-creation actions...
+# Restoring D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch37\BlazorWebAssembly\BlazorWebAssembly.csproj:
+#   Determining projects to restore...
+#   Restored D:\drs\ASPNETCore\Pro-ASP-NET-Core-7-Two\ch37\BlazorWebAssembly\BlazorWebAssembly.csproj (in 5.94 sec).
+# Restore succeeded.
+
+
+# Reference `..\DataModel\DataModel.csproj` added to the project.
