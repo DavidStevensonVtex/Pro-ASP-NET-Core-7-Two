ba938e38c0e204b07bd332cfa8bb916540f4de99
David Stevenson <drstevenson1958@yahoo.com>
2024-11-20 11:14:10 -0500

    Listing 38.10 The AdminPageModel.cs file in the Pages folder in the Advanced project

---
+++ ch38/Advanced/Pages/AdminPageModel.cs
@@ -0,0 +1,10 @@
+﻿// Listing 38.10 The AdminPageModel.cs file in the Pages folder in the Advanced project
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Advanced.Pages
+{
+    public class AdminPageModel : PageModel
+    {
+    }
+}
